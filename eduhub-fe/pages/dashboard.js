c0e27ad7c9e69d968eadb446faadbca11df8499a
AAFTABZAVERI <azaveri07@gmail.com>
2021-11-23 11:57:03 +0530

    dashboard

--- eduhub-fe/pages/dashboard.js
+++ eduhub-fe/pages/dashboard.js
@@ -1,19 +1,34 @@
 
 import { getSession } from 'next-auth/client';
-import styles from '../styles/Home.module.css';
-import {  signOut } from 'next-auth/client';
+import styles from '../styles/dashboard.module.css';
+import { signOut } from 'next-auth/client';
+
 
 export default function Homescreen({ user }) {
   return (
-    <div className={styles.container}>
-      <h1>EduHub Dashboard</h1>
+    <div>
+      <center><h2>EduHub Dashboard</h2> </center>
+
+      <div className={styles.container}>
+        <div className={styles.card}> Enterprise Computing 
+        </div>
+        <div className={styles.card}> Cloud Computing </div>
+        <div className={styles.card}> MEP </div>
+        <div className={styles.card}> DSS </div>
+        <div className={styles.card}> Enterprise Computing </div>
+        <div className={styles.card}> Cloud Computing </div>
+        <div className={styles.card}> MEP </div>
+        <div className={styles.card}> DSS </div>
+      </div>
       <p>
         Welcome to EduHub: <b>{user.name}</b>
       </p>
       <p>{user.email}</p>
       <button className={styles.primaryButton} onClick={() => signOut()}>
-            Sign Out
-          </button>
+        Sign Out
+      </button>
+      
+
     </div>
   );
 }
