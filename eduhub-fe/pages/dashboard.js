fc5d6ed02006f4ca0e76e86935f828d40791dc49
Riya6175 <soniriya243@gmail.com>
2021-11-24 23:09:22 +0530

    dashboard finish

--- eduhub-fe/pages/dashboard.js
+++ eduhub-fe/pages/dashboard.js
@@ -1,19 +1,27 @@
+import { useState,useEffect } from 'react';
 import style from '../styles/Home.module.css';
 import styles from '../styles/dashboard.module.css';
 
 
-
 export default function Dashboard({ user }) {
+
+  const [name,setName] = useState(0);
+  useEffect(() => {
+    if (window) { 
+      // set props data to session storage or local storage  
+      setName(sessionStorage.getItem("name"));
+    }
+}, []);
   return (
 
     <div className={style.container}>
       <div className={styles.navbar}>
-        <a> Home</a>
+        <a> Home </a>
         <div className={styles.navCentered}>
           <a> EduHub Dashboard</a>
         </div>
         <div className={styles.navbarRight}>
-          <a>name</a>
+          <a>{name}</a>
           <button className={styles.btn}>
               Sign Out
           </button>
@@ -22,29 +30,21 @@ export default function Dashboard({ user }) {
       <div className={styles.gridContainer}>
         <div className={styles.gridItem}>
           <div className={styles.class}>
-            <img src="/subject1.jpg"/>
-            <p className={styles.title}> Class Name</p>
-          </div>
-          <div className={styles.desc}>
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, </p>
+            <img className={styles.subjectImg} src="/subject1.jpg"/>
+            <div className={styles.other}>
+              <p className={styles.title}> Enterprise computing </p>
+              <p className={styles.prof}>Prof. PM Jatt</p>
+              <div className={styles.desc}>
+                <p className={styles.desc}>  consectetur adipiscing elit,Lorem ipsum dolor  sit amet, consectetur  </p>
+              </div>
+            </div>
+            <div>
+              <img className={styles.dots} src="/three-dots.png"/>
+            </div>
           </div>
         </div>
-        <div className={styles.gridItem}>Class name</div>
       </div>
     </div>
   );
 }
 
-// export async function getServerSideProps(context) {
-//   const session = await getSession(context);
-//   if (!session) {
-//     context.res.writeHead(302, { Location: '/' });
-//     context.res.end();
-//     return {};
-//   }
-//   return {
-//     props: {
-//       user: session.user,
-//     },
-//   };
-// }
