74a0c4cfc485d620c6786c08e779ea7d9d594d3c
Riya6175 <soniriya243@gmail.com>
2021-11-24 21:16:31 +0530

    dashboard & login

--- eduhub-fe/pages/dashboard.js
+++ eduhub-fe/pages/dashboard.js
@@ -1,58 +1,50 @@
-import { getSession } from 'next-auth/client';
-<<<<<<< HEAD
-import styles from '../styles/Home.module.css';
-=======
+import style from '../styles/Home.module.css';
 import styles from '../styles/dashboard.module.css';
-import { signOut } from 'next-auth/client';
 
->>>>>>> c0e27ad7c9e69d968eadb446faadbca11df8499a
+
 
 export default function Dashboard({ user }) {
   return (
-<<<<<<< HEAD
-    <div className={styles.container}>
-=======
-    <div>
-      <center><h2>EduHub Dashboard</h2> </center>
 
-      <div className={styles.container}>
-        <div className={styles.card}> Enterprise Computing 
+    <div className={style.container}>
+      <div className={styles.navbar}>
+        <a> Home</a>
+        <div className={styles.navCentered}>
+          <a> EduHub Dashboard</a>
         </div>
-        <div className={styles.card}> Cloud Computing </div>
-        <div className={styles.card}> MEP </div>
-        <div className={styles.card}> DSS </div>
-        <div className={styles.card}> Enterprise Computing </div>
-        <div className={styles.card}> Cloud Computing </div>
-        <div className={styles.card}> MEP </div>
-        <div className={styles.card}> DSS </div>
+        <div className={styles.navbarRight}>
+          <a>name</a>
+          <button className={styles.btn}>
+              Sign Out
+          </button>
+        </div>
       </div>
->>>>>>> c0e27ad7c9e69d968eadb446faadbca11df8499a
-      <p>
-        Welcome to Eduhub: <b>{user.name}</b>
-      </p>
-      <p>{user.email}</p>
-<<<<<<< HEAD
-=======
-      <button className={styles.primaryButton} onClick={() => signOut()}>
-        Sign Out
-      </button>
-      
-
->>>>>>> c0e27ad7c9e69d968eadb446faadbca11df8499a
+      <div className={styles.gridContainer}>
+        <div className={styles.gridItem}>
+          <div className={styles.class}>
+            <img src="/subject1.jpg"/>
+            <p className={styles.title}> Class Name</p>
+          </div>
+          <div className={styles.desc}>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, </p>
+          </div>
+        </div>
+        <div className={styles.gridItem}>Class name</div>
+      </div>
     </div>
   );
 }
 
-export async function getServerSideProps(context) {
-  const session = await getSession(context);
-  if (!session) {
-    context.res.writeHead(302, { Location: '/' });
-    context.res.end();
-    return {};
-  }
-  return {
-    props: {
-      user: session.user,
-    },
-  };
-}
+// export async function getServerSideProps(context) {
+//   const session = await getSession(context);
+//   if (!session) {
+//     context.res.writeHead(302, { Location: '/' });
+//     context.res.end();
+//     return {};
+//   }
+//   return {
+//     props: {
+//       user: session.user,
+//     },
+//   };
+// }
