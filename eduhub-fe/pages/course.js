31ba7064f79bb432dfa2cf7268451867d9185a1c
Riya6175 <soniriya243@gmail.com>
2021-11-26 22:50:19 +0530

    course

--- eduhub-fe/pages/course.js
+++ eduhub-fe/pages/course.js
@@ -6,7 +6,11 @@ export default function course() {
     return (
         <div>
             <NavBar />
-            <center><h1 className={styles.heading}>SUBJECT/COURSE NAME</h1> </center>
+            <div  className={styles.heading}>
+                <h1>SUBJECT/COURSE NAME</h1>
+                <p>Description Description Description Description Description Description Description</p>
+            </div>
+            
             <div>
                 <div className={styles.left}>
                     <div className={styles.chapter}>
@@ -14,6 +18,7 @@ export default function course() {
                         <p className={styles.p}> Topic 1: any kind of description</p>
                         <p className={styles.p}> Topic 2: any kind of description</p>
                         <p className={styles.p}> Topic 3: any kind of description</p>
+                        <p className={styles.add}> + Add Topic</p>
                         <hr />
                     </div>
 
@@ -22,16 +27,26 @@ export default function course() {
                         <p className={styles.p}> Topic 1: any kind of description</p>
                         <p className={styles.p}> Topic 2: any kind of description</p>
                         <p className={styles.p}> Topic 3: any kind of description</p>
+                        <p className={styles.add}> + Add Topic</p>
                         <hr />
                     </div>
 
                     <div className={styles.chapter}>
                         <h2> General </h2>
+                        <p className={styles.add}> + Add Topic</p>
                     </div>
                 </div>
                 <div className={styles.right}>
-                    <div className={styles.card1}> Assignments </div>
-                    <div className={styles.card2}> Quizzes </div>
+                    <div className={styles.card1}> 
+                        <h3>Assignments </h3>
+                        <p>Assignment 1</p>
+                        <p>Assignment 2</p>
+                    </div>
+                    <div className={styles.card2}> 
+                        <h3>Quizzes </h3>
+                        <p>Quiz 1</p>
+                        <p>Quiz 2</p>
+                    </div>
                 </div>
             </div>
         </div>
