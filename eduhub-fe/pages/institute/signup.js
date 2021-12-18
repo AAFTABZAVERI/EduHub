6af7ac4d2c21e4de9bf470f894d8342b2cbf7b6c
AAFTABZAVERI <azaveri07@gmail.com>
2021-12-18 14:47:56 +0530

    signup UI

--- eduhub-fe/pages/institute/signup.js
+++ eduhub-fe/pages/institute/signup.js
@@ -2,8 +2,9 @@ import axios from "axios"
 import Router from "next/router"
 import Link from "next/link"
 import { useEffect, useRef, useState } from "react"
+import styles from '../../styles/instituteSignUp.module.css';
 
-export default function signup(){
+export default function signup() {
 
     const [beforeSubmit, setbeforeSubmit] = useState(1)
 
@@ -15,47 +16,48 @@ export default function signup(){
         const currentname = name.current.value
         const currentdesc = description.current.value
         const currentemail = email.current.value
-        
+
         axios.post('http://127.0.0.1:5000/add-institute', {
-            "name":currentname,
+            "name": currentname,
             "description": currentdesc,
-            "email":currentemail
+            "email": currentemail
         })
-        .then(function (response) {
-            console.log(response);
-            setbeforeSubmit(0)
-            // Router.push('/dashboard')
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+            .then(function (response) {
+                console.log(response);
+                setbeforeSubmit(0)
+                // Router.push('/dashboard')
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
     const loginRedirect = () => {
         // Router.push('/login')
     }
 
-    return(
+    return (
         <div>
             {beforeSubmit ?
-                <div>
+                <div className={styles.container}>
                     <form>
-                        Institute Name<br/>
-                        <input ref={name} type="text" id="name" name="name" /><br/>
-                        Description<br/>
-                        <input ref={description} type="text" id="description" name="description" /><br/>
-                        Email Address:<br/>
-                        <input ref={email} type="text" id="email" name="email" /><br/><br/>
+                        <h1> Sign Up </h1>
+                        <label className={styles.lbl}>Institute Name</label>
+                        <input ref={name} type="text" id="name" name="name" className={styles.txtbox} /> <br /><br />
+                        <label className={styles.lbl}>Description </label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <input ref={description} type="text" id="description" name="description" className={styles.txtbox} /> <br /><br />
+                        <label className={styles.lbl}>Email Address</label> &nbsp;
+                        <input ref={email} type="text" id="email" name="email" className={styles.txtbox} /> <br /><br />
+                        <button onClick={instituteSignup} className={styles.btn}> Submit </button> <br /><br />
                     </form>
-                        <button onClick={instituteSignup}> Submit </button>
-                </div> 
+                    
+                </div>
                 :
                 <div>
-                            Your form is submitted,<Link href="login"> Login </Link>to access your portal.
-                            
+                    Your form is submitted,<Link href="login"> Login </Link>to access your portal.
                 </div>
             }
-            
+
         </div>
     )
 }
