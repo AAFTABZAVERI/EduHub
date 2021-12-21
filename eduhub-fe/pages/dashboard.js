8e781aae52f5f32920b3eea39b2028a5a7a53be6
Vishnu3125 <vinayakv3125@gmail.com>
2021-12-21 20:15:48 +0530

    small change

--- eduhub-fe/pages/dashboard.js
+++ eduhub-fe/pages/dashboard.js
@@ -29,7 +29,6 @@ export default function Dashboard({ user }) {
             <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, </p>
           </div>
         </div>
-        {/* <div className={styles.gridItem}>Class name</div> */}
       </div>
     </div>
   );
