67e256682f3a4687adf19313c7b4b7224a35115e
Raj Purohit <77795891+RajPurohit711@users.noreply.github.com>
2021-12-22 18:31:32 +0530

    delete users

--- eduhub-fe/pages/studentComponents/StudentDashboardComponent.js
+++ eduhub-fe/pages/studentComponents/StudentDashboardComponent.js
@@ -2,7 +2,7 @@ import style from '../../styles/Home.module.css';
 import styles from '../../styles/dashboard.module.css';
 import sdcStyle from './StudentDashboardComponent.module.css'
 import Router from 'next/router'
-
+import axios from 'axios';
 
 function StudentDashboardComponent(props) {
     function threeDots(Id){
@@ -16,8 +16,19 @@ function StudentDashboardComponent(props) {
   }
 
   function deleteCourse(Id){
+    axios.delete('http://127.0.0.1:5000/student-class/'+sessionStorage.getItem("Id"),{
+      data:{
+        "courseId":Id
+      }
+    })
+    .then(function (response) {
+      Router.reload(window.location.pathname)
+      console.log(response.data)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
     
-    Router.reload(window.location.pathname)
   }
 
     return (
@@ -36,7 +47,7 @@ function StudentDashboardComponent(props) {
               <div id={props.Id+'-model'} className={sdcStyle.modal}>
                 <div className={sdcStyle.modalContent}>
                   <span className={sdcStyle.close} onClick={() => closethreeDots(props.Id)}>&times;</span>
-                  <p>{props.name} {` Id : `+props.Id}</p>
+                  <p>{props.name} {` Id : `+ props.Id}</p>
                   <button onClick={() => deleteCourse(props.Id)}> Delete course </button>
                 </div>
               </div>
