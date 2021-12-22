95f514376b9eabe507aac3343c8726b42e0cf65c
Vishnu3125 <vinayakv3125@gmail.com>
2021-12-22 15:23:31 +0530

    student last

--- eduhub-fe/pages/dashboard.js
+++ eduhub-fe/pages/dashboard.js
@@ -2,39 +2,78 @@ import { useState, useEffect } from 'react';
 import style from '../styles/Home.module.css';
 import styles from '../styles/dashboard.module.css';
 import NavBar from '../components/navbar';
+import StudentDashboardComponent from './studentComponents/StudentDashboardComponent';
+import axios from 'axios';
 // import Popup from '../components/modalpopup';
 
 
 export default function Dashboard() {
-  return (
+  const [professorCourse, setprofessorCourse] = useState(0)
+  useEffect(() => {
+    axios.get('http://127.0.0.1:5000/faculty-classroom/'+sessionStorage.getItem("Id"))
+        .then(function (response) {
+          setprofessorCourse(response.data)
+          console.log(professorCourse)
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+  }, [])
 
+  function addButton(){
+    console.log("working")
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+  }
+
+  function addButtonClose(){
+    console.log("working")
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+  }
+
+  function addCourse(){
+    var courseName = document.getElementById("CourseName").value;
+    var courseDescription = document.getElementById("CourseDescription").value;
+
+    axios.post('http://127.0.0.1:5000/faculty-classroom/'+sessionStorage.getItem("Id"), {
+          "instituteId" : sessionStorage.getItem("instituteId"),
+          "facultyName": sessionStorage.getItem("name"),
+          "courseName" : courseName,
+          "courseDesc" : courseDescription
+        })
+        .then(function (response) {
+          console.log(response.data)
+          setprofessorCourse(response.data)
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+  }
+
+  return (
     <div className={style.container}>
-      {/* <div className={styles.navbar}>
-        <a> Home </a>
-        <div className={styles.navCentered}>
-          <a> EduHub Dashboard</a>
-        </div>
-        <div className={styles.navbarRight}>
-          <a>{name}</a>
-          <button className={styles.btn}>
-              Sign Out
-          </button>
-        </div>
-      </div> */}
       <NavBar />
       <div className={styles.gridContainer}>
-        <div className={styles.gridItem}>
-          <div className={styles.class}>
-            <img className={styles.subjectImg} src="/subject1.jpg" />
-            <div className={styles.other}>
-              <p className={styles.title}> Enterprise computing </p>
-              <p className={styles.prof}>Prof. PM Jatt</p>
-              <div className={styles.desc}>
-                <p className={styles.desc}>  consectetur adipiscing elit,Lorem ipsum dolor  sit amet, consectetur  </p>
-              </div>
-            </div>
+        {professorCourse ? professorCourse.map((course) => 
+        <StudentDashboardComponent name={course.name} faculty={course.faculty} id={course.Id} description={course.description}/> ) 
+        : 
+        <div> No course Available </div>}
+      </div>
+      <div>
+        <button onClick={addButton} >+</button>
+      </div>
+      <div>
+        <div id="myModal" className= {styles.modal}>
+          <div className={styles.modalContent}>
+          <span className={styles.close} onClick={addButtonClose}>&times;</span>
             <div>
-              <img className={styles.dots} src="/three-dots.png" />
+              <p style={{display:"inline-block", marginRight:"5px"}}>Course Name : </p>
+              <input type="text" name='CourseName' id='CourseName'></input><br></br>
+              <p style={{display:"inline-block", marginRight:"5px"}}>Course Description : </p>
+              <input type="text" name='CourseDescription' id='CourseDescription'></input><br></br>
+              <button onClick={addCourse}>Add Course</button>
             </div>
           </div>
         </div>
